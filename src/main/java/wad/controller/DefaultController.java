4f50ba27f77742b92d1ab2bff5d1ba35af73edc7
Erkki Heino <evsheino@gmail.com>
2014-10-31 17:17:04 +0200

    Travis

--- src/main/java/wad/controller/DefaultController.java
+++ src/main/java/wad/controller/DefaultController.java
@@ -1,8 +1,5 @@
 package wad.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
