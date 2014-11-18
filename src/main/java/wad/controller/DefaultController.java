f82a3c89aa02033fa2f29f30d26c907c3d9b0b53
Erkki Heino <evsheino@gmail.com>
2014-11-18 23:16:59 +0200

    Made Expense add form prettier

--- src/main/java/wad/controller/DefaultController.java
+++ src/main/java/wad/controller/DefaultController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import wad.domain.Expense;
 
 @Controller
 @RequestMapping("*")
@@ -11,6 +12,7 @@ public class DefaultController {
 
     @RequestMapping(method = RequestMethod.GET)
     public String viewIndex(Model model) {
+        model.addAttribute("expense", new Expense());
         return "index";
     }
 
